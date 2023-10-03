be2d03d2b01546080b0b8c52ecf2d9c35d586264
Tanner Silva <t@tannersilva.com>
2023-10-03 03:14:31 -0500

    no longer hashing zero length data

--- Sources/SwiftBlake2/Blake2bHasher.swift
+++ Sources/SwiftBlake2/Blake2bHasher.swift
@@ -8,67 +8,69 @@ public struct Blake2bHasher {
 		try newHasher.update(bytes:bytes)
 		return try newHasher.export()
 	}
-    
-    fileprivate static func validateOutputLength(_ olen:size_t) throws {
-    	guard olen > 0 && olen <= 64 else {
-    		throw Blake2bError.invalidOutputLength
-    	}
-    }
-    
-    public enum Blake2bError:Swift.Error {
-    	case invalidOutputLength
-        case initializationError
-        case updateError
-        case exportError
-    }
+	
+	fileprivate static func validateOutputLength(_ olen:size_t) throws {
+		guard olen > 0 && olen <= 64 else {
+			throw Blake2bError.invalidOutputLength
+		}
+	}
+	
+	public enum Blake2bError:Swift.Error {
+		case invalidOutputLength
+		case initializationError
+		case updateError
+		case exportError
+	}
 
-    fileprivate var state = blake2b_state()
-    
-    public let outputLength:size_t
+	fileprivate var state = blake2b_state()
+	
+	public let outputLength:size_t
 
-    /// Initialize a new blake2b hasher
-    public init(outputLength:size_t) throws {
-    	try Self.validateOutputLength(outputLength)
-        guard blake2b_init(&state, outputLength) == 0 else {
-            throw Blake2bError.initializationError
-        }
-        self.outputLength = outputLength
-    }
-        
-    /// Update the hasher with new data
-    public mutating func update<C>(bytes input:C) throws where C:Collection, C.Element == UInt8 {
-       	if let getThing = try input.withContiguousStorageIfAvailable({ someBytes in
-       		let baseaddr = UnsafeRawPointer(someBytes.baseAddress!)
-			guard blake2b_update(&state, baseaddr, input.count) == 0 else {
-				throw Blake2bError.updateError
-			}
-       	}) {
-       		return getThing
-       	} else {
-			let buffer = UnsafeMutableBufferPointer<UInt8>.allocate(capacity: input.count)
-			defer { buffer.deallocate() }
-			_ = buffer.initialize(from: input)
-			guard blake2b_update(&state, UnsafeRawPointer(buffer.baseAddress!), input.count) == 0 else {
-				throw Blake2bError.updateError
+	/// Initialize a new blake2b hasher
+	public init(outputLength:size_t) throws {
+		try Self.validateOutputLength(outputLength)
+		guard blake2b_init(&state, outputLength) == 0 else {
+			throw Blake2bError.initializationError
+		}
+		self.outputLength = outputLength
+	}
+		
+	/// Update the hasher with new data
+	public mutating func update<C>(bytes input:C) throws where C:Collection, C.Element == UInt8 {
+		if input.count > 0 {
+			if let getThing = try input.withContiguousStorageIfAvailable({ someBytes in
+				let baseaddr = UnsafeRawPointer(someBytes.baseAddress!)
+				guard blake2b_update(&state, baseaddr, input.count) == 0 else {
+					throw Blake2bError.updateError
+				}
+			}) {
+				return getThing
+			} else {
+				let buffer = UnsafeMutableBufferPointer<UInt8>.allocate(capacity: input.count)
+				defer { buffer.deallocate() }
+				_ = buffer.initialize(from: input)
+				guard blake2b_update(&state, UnsafeRawPointer(buffer.baseAddress!), input.count) == 0 else {
+					throw Blake2bError.updateError
+				}
 			}
 		}
-    }
+	}
 
-    /// Finish the hashing
-    public mutating func export() throws -> Data {
-        let finalHash = UnsafeMutableBufferPointer<UInt8>.allocate(capacity:outputLength)
-        defer {
-            finalHash.deallocate()
-        }
-        guard blake2b_final(&state, finalHash.baseAddress!, outputLength) == 0 else {
-            throw Blake2bError.updateError
-        }
-        return Data(buffer:finalHash)
-    }
-    
-    mutating func reset() throws {
-        guard blake2b_init(&state, outputLength) == 0 else {
-            throw Blake2bError.updateError
-        }
-    }
+	/// Finish the hashing
+	public mutating func export() throws -> Data {
+		let finalHash = UnsafeMutableBufferPointer<UInt8>.allocate(capacity:outputLength)
+		defer {
+			finalHash.deallocate()
+		}
+		guard blake2b_final(&state, finalHash.baseAddress!, outputLength) == 0 else {
+			throw Blake2bError.updateError
+		}
+		return Data(buffer:finalHash)
+	}
+	
+	mutating func reset() throws {
+		guard blake2b_init(&state, outputLength) == 0 else {
+			throw Blake2bError.updateError
+		}
+	}
 }
